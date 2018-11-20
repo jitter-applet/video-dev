2c69aca1a7f1bec2032c57ecbdff78544dff3ab3
SunAo <572220216@qq.com>
2018-11-20 22:21:50 +0800

    merge

--- zzuisa-videos-dev/zzuisa-videos-dev-api/src/main/java/cn/zzuisa/controller/UserController.java
+++ zzuisa-videos-dev/zzuisa-videos-dev-api/src/main/java/cn/zzuisa/controller/UserController.java
@@ -1,90 +1,59 @@
 package cn.zzuisa.controller;
 
-import java.util.UUID;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import cn.zzuisa.constant.RCons;
-import cn.zzuisa.pojo.Users;
-import cn.zzuisa.pojo.vo.UsersVO;
 import cn.zzuisa.service.UserService;
-import cn.zzuisa.utils.MD5Utils;
 import cn.zzuisa.utils.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@Api(value = "用户注册登录的接口", tags = { "注册和登录的controller" })
+@Api(value = "用户相关业务的接口", tags = { "用户相关业务的controller" })
 public class UserController extends BasicController {
 
 	@Autowired
 	private UserService userService;
 
-	@ApiOperation(value = "用户注册", notes = "用户注册的接口")
-	@PostMapping("/regist")
-	public R regist(@RequestBody Users user) throws Exception {
-		// 1. 判断用户名和密码必须不为空
-		if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())) {
-			return R.errorMsg(RCons.VALIDLOGIN);
-		}
-		// 2. 判断用户名是否存在
-		boolean usernameIsExist = userService.queryUsernameIsExist(user.getUsername());
-		// 3. 保存用户注册信息
-		if (!usernameIsExist) {
-			user.setNickname(user.getUsername());
-			user.setPassword(MD5Utils.getMD5Str(user.getPassword()));
-			user.setFansCounts(0);
-			user.setReceiveLikeCounts(0);
-			user.setFollowCounts(0);
-			userService.saveUser(user);
-		} else {
-			return R.errorMsg(RCons.USERHASEXIST);
-		}
-		user.setPassword("");
-		UsersVO userVO = setUserRedisSessionToken(user);
-		return R.ok(userVO);
-	}
-
-	public UsersVO setUserRedisSessionToken(Users userModel) {
-		String uniqueToken = UUID.randomUUID().toString();
-		redis.set(USER_REDIS_SESSION + ":" + userModel.getId(), uniqueToken, 1000 * 60 * 30);
-		UsersVO userVO = new UsersVO();
-		BeanUtils.copyProperties(userModel, userVO);
-		userVO.setUserToken(uniqueToken);
-		return userVO;
-	}
-
-	@ApiOperation(value = "用户登录", notes = "用户登录的接口")
-	@PostMapping("/login")
-	public R login(@RequestBody Users user) throws Exception {
-		String username = user.getUsername();
-		String password = user.getPassword();
-		// 判断用户名和密码不为空Ï
-		if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
-			R.errorMsg(RCons.VALIDLOGIN);
-		}
-		password = MD5Utils.getMD5Str(password);
-		Users userResult = userService.querryUsersForLogin(username, password);
-		if (userResult != null) {
-			userResult.setPassword("");
-			UsersVO userVO = setUserRedisSessionToken(userResult);
-			return R.ok(userVO);
-		} else {
-			return R.errorMsg(RCons.ERRORLOGIN);
-		}
-	}
-
-	@ApiOperation(value = "用户注销", notes = "用户注销的接口")
+	@ApiOperation(value = "用户上传头像", notes = "用户上传头像的接口")
 	@ApiImplicitParam(name = "userId", value = "用户id", required = true, dataType = "String", paramType = "query")
-	@PostMapping("/logout")
-	public R logout(String userId) {
-		redis.del(USER_REDIS_SESSION + ":" + userId);
+	@PostMapping("/uploadFace")
+	public R uploadFace(String userId, @RequestParam("file") MultipartFile[] files) throws Exception {
+		//文件保存的命名空间
+		String fileSpace = "D:/wx-videos";
+		String uploadPathDB = "/" +userId + "/face";
+		if(files!=null&&files.length>0){
+			FileOutputStream fileOutputStream = null;
+			InputStream inputStream = null;
+			String fileName = files[0].getOriginalFilename();
+			if(StringUtils.isNotBlank(fileName)){
+				//文件上传的最终保存路径
+				String finalFacePath = fileSpace + uploadPathDB +"/" + fileName;
+				//设置数据库保存的路径
+				uploadPathDB +=("/"+fileName);
+				File outFile = new File(finalFacePath);
+				
+				if(outFile.getParentFile()!=null || outFile.getParentFile().isDirectory()){
+					outFile.getParentFile().mkdirs();
+				}
+				
+				fileOutputStream = new FileOutputStream(outFile);
+				inputStream = files[0].getInputStream();
+				IOUtils.copy(inputStream, fileOutputStream);
+			}
+		}
 		return R.ok(RCons.LOGOUTOK);
 	}
 
